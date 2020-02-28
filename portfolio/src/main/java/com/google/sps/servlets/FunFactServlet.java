ebfb768cc4e130b2594e24406096958bcee71692
Elvis Adorkor <elvisedem98@gmail.com>
2020-02-28 02:28:33 -0500

    step 2 done

--- portfolio/src/main/java/com/google/sps/servlets/FunFactServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FunFactServlet.java
@@ -15,18 +15,38 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
-@WebServlet("/data")
-public class DataServlet extends HttpServlet {
+/** Servlet that returns a fun fact. */
+@WebServlet("/fun-facts")
+public final class FunFactServlet extends HttpServlet {
+
+  private List<String> funFacts;
+
+  @Override
+  public void init() {
+    funFacts = new ArrayList<>();
+    funFacts.add(
+        "I have never shaved my moustache");
+    funFacts.add("I got my first computer when I was 18");
+    funFacts.add("I am super scared of heights, almost passed out a six flags last summer");
+    funFacts.add("Cereal before milk, people");
+    funFacts.add("I saw Jaden Smith, Young Thug and J Cole in one night");
+    funFacts.add("I have seen Khalid live too!");
+    funFacts.add("I am so bad at swimming");
+    funFacts.add("I'm a bad dancer too");
+  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String funFact = funFacts.get((int) (Math.random() * funFacts.size()));
+
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Elvis Adorkor!</h1>");
+    response.getWriter().println(funFact);
   }
 }

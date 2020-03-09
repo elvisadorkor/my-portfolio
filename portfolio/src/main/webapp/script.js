16cb6f98f7e2ba1d6837f70b14e0dadd1f6a56b8
Elvis Adorkor <elvisedem98@gmail.com>
2020-03-08 21:06:15 -0400

    added some markers for now. will add more interactive features

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -36,7 +36,112 @@ function createListElement(comment) {
 }
 
 function createMap() {
-  const map = new google.maps.Map(
+  var map = new google.maps.Map(
       document.getElementById('map'),
-      {center: {lat: 48.8566, lng: 2.3522}, zoom: 16});
+      {center: {lat: 54.5260, lng: 15.2551}, 
+      zoom: 3, 
+      styles: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ]});
+//     var Paris = new google.maps.Marker({
+//     position: {lat: 48.8566, lng: 2.3522},
+//     map: map,
+//     animation: google.maps.Animation.DROP,
+//     title: 'Paris'
+//   });
+    var cities = [
+        ['Paris', 48.8566, 2.3522],
+        ['London', 51.5074, 0.1278],
+        ['Barcelona', 41.3851, 2.1734],
+        ['Copenhagen', 55.6761, 12.5683],
+        ['Santorini', 36.3932, 25.4615],
+        ['Florence', 43.7696, 11.2558],
+        ['Amsterdam', 52.3667, 4.8945],
+        ['Prague', 50.0755, 14.4378]
+];
+    var i = 0;
+    for(i = 0; i < cities.length; i++){
+        var marker = new google.maps.Marker({
+            position: {lat: cities[i][1], lng: cities[i][2]},
+            map: map,
+            title: cities[i][0]
+        });
+    }
 }
